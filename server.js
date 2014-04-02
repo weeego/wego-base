33be57b98ba25e898f99818f15bf68ef3ec2f8c2
zuaa@163.com <zuaa@163.com>
2014-04-02 23:49:45 +0800

    初始化nodejs的html框架

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-var http = require('http');
-var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
-}).listen(port);
+var express = require('express')
+var app = express();
+ 
+app.configure(function(){
+    app.set('port', 18080);
+    app.set('views', __dirname + '/views');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('expressdemo'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.errorHandler());
+});
+ 
+app.get('/ejs/:str', function(request, response){
+    var testStr = request.params.str;
+    response.render('ejstest.ejs', {title: testStr});
+});
+ 
+app.get('/jade/:str', function (request, response) {
+    var testStr = request.params.str;
+    response.render('jadetest.jade', {title: testStr});
+});
+ 
+app.listen(18080);
